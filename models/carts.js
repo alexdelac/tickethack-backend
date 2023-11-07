85e9ac68ad537ccf08f7caba0b06d5b279ad8506
Marie <mme.sabre@outlook.fr>
2023-11-07 17:34:39 +0100

    ajout schema + route carts

--- models/carts.js
+++ models/carts.js
@@ -3,4 +3,13 @@ const mongoose = require('mongoose')
 
 // ajouter Schema 
 
+const cartSchema  = mongoose.Schema ({
+    trip: {type: mongoose.Schema.Types.ObjectId, ref: 'trips'},
+});
+
+const Cart = mongoose.model('carts', cartSchema);
+
+
+
 //Attention ne pas oublier l'export
+module.exports = Cart;
--- routes/carts.js
+++ routes/carts.js
@@ -2,14 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 // ajout require vers le model necessaire
+const Cart = require('../models/carts');
 
 const fetch = require('node-fetch');
 
 
-
-
-
 //ajout des routes
+
+
+router.post('/',(req, res) => {
+    const newCart = new Cart({
+        trips: req.body.id
+    })
+    newCart.save().then((data) => {
+        res.json({result: true});
+    })
+
+})
 
 
 
