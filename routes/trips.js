c7fbd259e0af37f148fe2434112e3982700bda4a
Alexandre Delacre <delacre.alex@gmail.com>
2023-11-08 16:26:48 +0100

    gestion de la casse

--- routes/trips.js
+++ routes/trips.js
@@ -12,12 +12,13 @@ const fetch = require('node-fetch');
 //ajout des routes
 router.post('/',(req, res) => {
     const dateinf = new Date(req.body.date)
-   let dateSupTimestamp = new Date(req.body.date)
-     dateSupTimestamp.setDate(dateSupTimestamp.getDate()+1)
+     let dateSupTimestamp = new Date(req.body.date)
+    dateSupTimestamp.setDate(dateSupTimestamp.getDate()+1)
     const dateSup = new Date(dateSupTimestamp)
-    console.log(dateinf)
-   console.log(dateSup)
-Trip.find({departure: req.body.departure, arrival: req.body.arrival, date: {$gte: dateinf, $lt: dateSup}})
+   let depart = req.body.departure.split('').slice(0, 1).join().toUpperCase()+ req.body.departure.split('').slice(1).join('').toLowerCase()
+   let arrival = req.body.arrival.split('').slice(0, 1).join().toUpperCase()+ req.body.arrival.split('').slice(1).join('').toLowerCase()
+   console.log(depart)
+Trip.find({departure: depart, arrival: arrival, date: {$gte: dateinf, $lt: dateSup}})
 .then(data => {
     res.json({trips: data});
 
